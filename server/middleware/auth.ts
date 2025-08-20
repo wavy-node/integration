7372285882a6296836d81f2203b0d64069bce6ab
Manuel Rivera <menu.rivera@gmail.com>
2025-08-19 23:16:45 -0600

    feat: integrated @wavynode/utils package

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,9 +1,16 @@
+import { validateSignature } from "@wavynode/utils";
 import { createError, eventHandler, getHeader, readBody } from "h3";
 import { IWebhookBody } from "~~/types/webhook-body.type";
-import { createHmacSignature } from "~~/util/hmac";
 
-// todo: verify GET requests
 export default eventHandler(async e => {
+	const [path, _] = e.path.split('?')
+
+	// this template only accepts GET and POST requests 
+	if (e.method != 'GET' && e.method != 'POST') throw createError({
+		status: 405,
+		statusMessage: 'Method Not Allowed',
+	})
+
 	// read body
 	const hmacHeader = getHeader(e, 'x-wavynode-hmac')
 	if (!hmacHeader) throw createError({
@@ -12,12 +19,27 @@ export default eventHandler(async e => {
 		message: 'Signature missing'
 	})
 
-	const body = await readBody<IWebhookBody>(e).catch(() => null)
-	if (!body) throw createError({ status: 400, message: 'No body provided' })
+	const timestamp = getHeader(e, 'x-wavynode-timestmap')
+	if (!timestamp) throw createError({
+		status: 400,
+		statusMessage: 'Bad Request',
+		message: 'Timestamp missing'
+	})
 
-	// assert it's a request from wavynode using our secret
-	const signature = createHmacSignature(JSON.stringify(body), process.env.SECRET)
-	if (signature !== hmacHeader) throw createError({
+	const body = await readBody<IWebhookBody>(e).catch(() => ({}))
+
+	const isValid = validateSignature({
+		method: e.method,
+		path,
+		body,
+		timestamp: parseInt(timestamp),
+		secret: process.env.SECRET,
+		// 5 minutes
+		timeTolerance: 300_000,
+		signature: hmacHeader
+	})
+
+	if (!isValid) throw createError({
 		status: 401,
 		statusMessage: 'Unauthorized',
 		message: 'Invalid signature'
